a5e1f7a08798d4e07837e97cc8ed7e3188e0e17c
ubaiidraza <154611273+ubaiidraza@users.noreply.github.com>
2024-04-30 10:10:17 +0500

    create js file

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+// IF Else statment assignment
+
+// QUESTION: 1
+
+// var num = + prompt ();
+// if(num >= 3   ){
+//     console.log('This value is divisible ')
+// }
+// else{
+//     console.log('This value not divisible ');
+// }
+
+
