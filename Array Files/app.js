d21b8f0f14230f2e74555b02ffa81b613e6a49a6
ubaiidraza <154611273+ubaiidraza@users.noreply.github.com>
2024-05-07 17:40:30 +0500

    complete array file

---
+++ Array Files/app.js
@@ -0,0 +1,158 @@
+
+// Array Files
+
+// 1. Declare an empty array using JS literal notation to store
+// student names in future.
+
+// let futureStudents = new Array();
+
+// 2. Declare an empty array using JS object notation to store
+// student names in future.
+
+// let futureStudent = [];
+
+// 3. Declare and initialize a strings array
+
+// let studenName = ['ubaid'];
+
+// 4. Declare and initialize a numbers array.
+
+// let favNumber = [10, 9, 8, 7, 6,];
+// console.log(favNumber);
+
+// 5. Declare and initialize a boolean array.
+
+// let boolean = [true, false]
+// console.log(boolean);
+
+// 6. Declare and initialize a mixed array.
+
+// let mixedArray = ['string', 9 , true, false];
+
+// 7. Declare and Initialize an array and store available mobile
+// networks in Pakistan.
+
+// let AvailableNetwork = ["Zong", "Ufone", "Jazz", "Telenor"];
+// console.log(AvailableNetwork);
+
+// 8. Declare and Initialize an array and store available education
+// qualifications in Pakistan (e.g. SSC, HSC, BCS, BS, BCOM, MS,
+// M. Phil., PhD). Show the listed qualifications in your browser
+// like:
+
+// let qualifications = ['EG', 'SSC', 'HSC', 'BSC', 'BS', 'BCOM',] 
+// let ul = document.querySelector('ul')
+
+// for(let i = 1; i <= qualifications.length; i++){
+//     ul.innerHTML += `<li>${qualifications[i]}</li>`
+// }
+
+// 9. Declare and initialize an empty array to store top movies of
+// 2015. Add movies one by one in an array. Display the elements
+// & length of array in your browser. (Use array’s length method)
+
+//  let array = ["Avengers' age of Ultern", "Spectr", "Inside Out", "Jurrasic World"]
+//  console.log(array);
+//  console.log(array.length);
+
+// 10. Declare and Initialize an array with your favorite cars. Show
+// a. First index of the array
+// b. Car at first index of the array
+// c. Last index of the array
+
+// let favorite_cars = ["Tesla Model S", "Porsche 911", "Lamborghini Aventador", "BMW M5"]
+
+// console.log(`First index of the array: ${favorite_cars.indexOf('Tesla Model S')}`);
+// console.log(`Car at first index of the array: ${favorite_cars[0]}`);
+// console.log(`Last index of the array ${favorite_cars.indexOf("BMW M5")}`);
+// console.log(`Last index of the array: ${favorite_cars[3]}`)
+
+// 11. Write a program to store 3 student names in an array. Take
+// another array to store score of these three students. Assume
+// that total marks are 500 for each student, display the scores &
+// percentages of students like:
+
+// let totalScore = 500;
+// let studenName = ['ubaid', 'saad', 'arham'];
+// let obtainScore = [480, 320, 280];
+// let percentages = [96, 46, 78]
+
+// console.log(`Score of ${studenName[0]} is ${obtainScore[0]} Percentage ${percentages[0]} %`);
+// console.log(`Score of ${studenName[1]} is ${obtainScore[1]} Percentage ${percentages[1]} %`);
+// console.log(`Score of ${studenName[2]} is ${obtainScore[2]} Percentage ${percentages[2]} %`);
+
+// 13. Write a program to store student scores in an array & sort
+// the array in ascending order using Array’s sort method. 
+
+// let studentScore = ["320", "230", "480", "120"];
+// console.log(studentScore.sort());
+
+// 14. Write a program to sort the below mentioned array:
+// var fruits = [“strawberry”, “apple”, “orange”, “banana”];
+
+// let fruits = ["stawbery", "Apple", "Orange", "Banana"];
+
+// console.log(`Fruits list ${fruits}`);
+// console.log(`Order list ${fruits.sort()}`);
+
+// 15. Write a program to initialize an array with city names. Copy
+// 3 array elements from cities array to selectedCities array. 
+
+// let cities = ['Karcahi', 'Lahore', 'Islamabad', 'peshawer', 'Quota']
+
+// console.log(`Cities list ${cities}`);
+// console.log(`Selected Cities ${cities[1]},${cities[4]}`);
+
+// 16. Write a program to create a single string from the below
+// mentioned array:
+// var arr = [“This ”, “ is ”, “ my ”, “ cat”];
+// (Use array’s join method)
+
+// let paragraf = ['this', 'is', 'my', 'cat']
+// let text = paragraf.join(' ');
+
+// console.log(`Array: ${paragraf}`);
+// console.log(`String: ${text}`);
+
+// 17. Create a new array. Store values one by one in such a way
+// that you can access the values in the order in which they were
+// stored. (FIFO-First In First Out)
+
+// let compPart = ["Keyboard", "Mouse", "Printer", "Monitor"];
+
+// console.log(`Out:
+// ${compPart[0]}`);
+// console.log(`Out:
+// ${compPart[1]}`);
+// console.log(`Out:
+// ${compPart[2]}`);
+// console.log(`Out:
+// ${compPart[3]}`);
+
+// 18. Create a new array. Store values one by one in such a way
+// that you can access the values in reverse order. (Last In-First
+// Out)
+
+// let Devices = ["Keyboard", "Mouse", "Printer", "Monitor"];
+
+// console.log(Devices.reverse());
+// console.log(`Out: ${Devices[0]}`);
+// console.log(`Out: ${Devices[1]}`);
+// console.log(`Out: ${Devices[2]}`);
+// console.log(`Out: ${Devices[3]}`);
+
+// 20. Declare and initialize an empty multidimensional array.
+// (Array of arrays)
+
+// let matrix = [[],[]]
+// console.log(matrix);
+
+// 21. Declare and initialize a multidimensional array representing
+// the following matrix:
+
+// let matrix = [
+//     [2, 8, 9]
+//     [3, 6, 9]
+// ];
+
+// console.log(matrix);
