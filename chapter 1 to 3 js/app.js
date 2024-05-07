68b356f1f4d7209f025824146ad0664fd69bc4df
ubaiidraza <154611273+ubaiidraza@users.noreply.github.com>
2024-05-07 17:47:51 +0500

    chspter 1 to 3 complete

---
+++ chapter 1 to 3 js/app.js
@@ -0,0 +1,101 @@
+// ==== Chapter 01 ====
+
+
+// ==== Chapter 01 Q 1 ====
+// alert('Error! Please enter a valid password.')
+
+// ==== Chapter 01 Q 2 ====
+// alert('Wellcome to JS land\nHappy Coding.')
+
+// ==== Chapter 01 Q 3 ====
+// alert('Welcome to js Land...')
+
+// ==== Chapter 01 Q 4 ====
+// var message = 'Hello... I can run JS through my werb browers console.';
+// alert(message)
+
+
+// ==== Chapter 02 ====
+
+
+// ==== Chapter 02 Q 1 2 3 4  ====
+// var userName = 'ubaid'
+// var age = '16 years old';
+// var info = 'Certified Web and app development';
+
+// alert(userName);
+// alert(age);
+// alert(info);
+
+// ==== Chapter 02 Q 5  ====
+// var food = 'PIZZA\nPIZZ\nPIZ\nPI\nP'
+// alert(food)
+
+// ==== Chapter 02 Q 6  ====
+// var email = 'My email address is haseeb@gmail.com'
+// alert(email)
+
+// ==== Chapter 02 Q 7  ====
+// var book = 'I am trying to leran from the book A smarter\nway to learn javascript'
+// alert(book)
+
+// ==== Chapter 02 Q 8  ====
+// var htmlContent = 'Hye! I can write Html content Throught JavaScipt'
+// alert(htmlContent)
+
+// ==== Chapter 02 Q 9  ====
+
+// var jsBoder = '“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”'
+// alert(jsBoder)
+
+
+// ==== Chapter 03 ====
+
+
+// ==== Chapter 03  Q 1 ====
+// var age = 16;
+// alert("i am " + age + " years old");
+
+// ==== Chapter 03  Q 2 ====
+// var hisHer = 2;
+// alert("you have visted this site " + hisHer + " times");
+
+// ==== Chapter 03  Q 3 ====
+// var birthYear = 2007;
+// alert("My birth year " + birthYear);
+
+// ==== Chapter 03  Q 4 ====
+// var visitorsName = "ubaid";
+// var productTilte = "on XYZ Clothing store"
+// var quantity = " ordered 5 T-shirt(s)"
+
+// alert(visitorsName + '' + quantity + '' + productTilte);
+
+// ==== Chapter 04  Q 1 ====
+
+//1.Declare 3 variables in one statement.
+
+// a, b, c, = 10
+
+// 2.Declare 5 legal & 5 illegal variable names
+
+// === legal Name ===
+
+// var userName = "ubaid";
+// var my_alue = 60;
+// var myNumber = 8;
+// var $myCast = "saad";
+// var data1 = 100;
+
+// === illegal Name ===
+
+// var 123 = '';
+// var @userName = "";
+// var username = "";
+
+// 3.Display this in your browser
+
+// document.write("Variable names can only contain,number, $ and, _. for Example : $my_firstName")
+// Document.write("Variables must begin with a $ or _,  or. For example $name, _name or name")
+// document.write("Variable names are case Sensitive")
+// document.write("Variable names should not be JS keywords")
